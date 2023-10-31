fc0242623e619c8b77738389b3918b640bcf54ff
Rachid Marir <marirrachid@yahoo.fr>
2023-10-31 02:30:02 +0100

    change chevron

--- src/components/ApartmentDescription.jsx
+++ src/components/ApartmentDescription.jsx
@@ -6,7 +6,7 @@ export function ApartmentDescription(){
             <div className='apartment__description'> 
             <p className='description__header'>
                 <span>Description</span>
-                <i className="fas fa-chevron-down"></i>
+                <i className="fas fa-chevron-up"></i>
             </p>
             <p className='description__content'>
                 Ita nobilium cum manibus haec nuptias aurum offertur usque nuptias Spoletium usque ubi tutius dextris sunt Spoletium ad nobilium tamen haec manibus vigore offertur dextris cavatis instituta inminuto ad offertur offertur ad vel artuum inminuto pergunt nuptias vel ubi nobilium dextris vel rogati manibus artuum ita sunt nobilium vigore ubi.    
