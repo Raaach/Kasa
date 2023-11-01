078dcbcf3d031d3ad8ae78251d7cf498c31ab06b
Rachid Marir <marirrachid@yahoo.fr>
2023-11-01 15:19:59 +0100

    used fetch in ApartGrid

--- src/components/ApartmentCard.jsx
+++ src/components/ApartmentCard.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import {Link} from "react-router-dom"
 import "./ApartmentCard.scss"
 
-function ApartmentCard() {
+function ApartmentCard(props) {
   return (
     <Link to="/flat">
       <div className='apartment'> 
-        <div className='apartment__subtitle'>Titre de la location</div>
+        <img src={props.imageUrl} alt=""/>
+        <div className='apartment__subtitle'>{props.title}</div>
       </div>
     </Link>
   )
--- src/components/AppartGrid.jsx
+++ src/components/AppartGrid.jsx
@@ -1,22 +1,25 @@
- import React from 'react'
+ import React, { useEffect, useState } from 'react'
  import "./AppartGrid.scss"
  import ApartmentCard from "./ApartmentCard.jsx"
 
  
  function AppartGrid() {
-   fetch("db.json")
-    .then((res) => res.json()) 
-    .then((res)=>console.log(res.length)) //on a utilisé fetch pour faire aparaitre les infos en json/se n'est pas le même res 
-    .catch((err) => console.error(err))
+  const [apartments, setApartments]= useState([])
+
+  useEffect(fetchApartments,[]) // useEffect avec array wide = excute seulement au chargement 
+
+  function fetchApartments(){
+    fetch("db.json")            //on a utilisé fetch pour faire aparaitre les infos en json/se n'est pas le même res 
+      .then((res) => res.json()) 
+      .then((res)=>setApartments(res)) 
+      .catch((err) => console.error(err))
+  }
    return (
-     <div className='grid'>
-     <ApartmentCard/>
-     <ApartmentCard/>
-     <ApartmentCard/>
-     <ApartmentCard/>
-     <ApartmentCard/>
-     <ApartmentCard/>
-     </div>
+    <div className='grid'>
+      {apartments.map((apartment)=>(
+        <ApartmentCard title={apartment.title} imageUrl= {apartment.cover}/>  //apartment est un gros objet qui contient title, tags etc... //on fait appelle cette ligne pour avoir plusieur apartement au lieu de tout écrire à la main chaque div 
+      ))}
+    </div>
    )
  }
  
