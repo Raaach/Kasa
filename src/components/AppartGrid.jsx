fd88e5afe74943655accd76abe1e9f1276206810
Rachid Marir <marirrachid@yahoo.fr>
2023-11-01 13:59:24 +0100

    add db.json

--- src/components/AppartGrid.jsx
+++ src/components/AppartGrid.jsx
@@ -1,8 +1,13 @@
  import React from 'react'
  import "./AppartGrid.scss"
  import ApartmentCard from "./ApartmentCard.jsx"
+
  
  function AppartGrid() {
+   fetch("db.json")
+    .then((res) => res.json()) 
+    .then((res)=>console.log(res.length)) //on a utilisé fetch pour faire aparaitre les infos en json/se n'est pas le même res 
+    .catch((err) => console.error(err))
    return (
      <div className='grid'>
      <ApartmentCard/>
